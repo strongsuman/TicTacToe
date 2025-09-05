ac6604f4f68c18d2e90d914e7cbc7b5736b57629
Jyoti Suman <jyotisuman866@gmail.com>
2025-09-06 04:32:09 +0530

    Removed Operators files

--- Operators.java
+++
@@ -1,9 +0,0 @@
-
-public class Operators{
-    public static void main(String[] args)  {
-      //  System.out.println(10/0);
-        System.out.println(10/0.0);
-       // System.out.println(0/0);
-        System.out.println(0.0/0);
-    }
-}
